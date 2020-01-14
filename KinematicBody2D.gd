e2a5039dc6476ec35cc2c2340ea2c1da19d72ad8
Ulisses <ulissescasemiro@gmail.com>
2020-01-14 00:38:55 -0300

    Colision and gravity

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,22 +3,30 @@ extends KinematicBody2D
 var motion = Vector2 ()
 var direction = "Right"
 var new_direction =  "Right"
+var sprite_scale = Vector2 (0.5,0.5)
+var sprite_offset =  -64
 #func FlipSprite(right):
 #	if right :
 #		$AnimatedSprite.set_scale(1,1)
 #	else: 
-#		$AnimatedSprite.set_scale(-1,1)
-
-
+#		$AnimatedSprite.set_scale(-1,1
 
 func _process(delta):
+	motion.x = 0 
+	if motion.y < 500 :
+		motion.y += 10
 	if  Input.is_action_pressed("ui_right"):
-		motion.x = 256
-	else:
-		if Input.is_action_pressed("ui_left"):
-			motion.x = -256
-		else: 
-			motion.x = 0 
+		motion.x = motion.x+256
+	
+	if Input.is_action_pressed("ui_left"):
+		motion.x = motion.x-256
+	
+	if is_on_floor() : 
+		print ("on floor")
+		if Input.is_action_pressed("ui_up"): 
+			motion.y = -400
+		
+	
 	
 	if motion.x == 0:
 		$AnimatedSprite.play("Idle")
@@ -32,8 +40,11 @@ func _process(delta):
 		new_direction = direction
 		if direction == "Right":
 			$AnimatedSprite.set_scale(Vector2(0.5,0.5))
+			$AnimatedSprite.offset.x = 96
 		if direction == "Left":
 			$AnimatedSprite.set_scale(Vector2(-0.5,0.5))
+			$AnimatedSprite.offset.x = 32
+			
 	
 	move_and_slide(motion)
 	pass
